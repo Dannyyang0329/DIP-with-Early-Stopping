c8ed88ef00202668c35181556dc6b3983b1bb3ed
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2024-05-23 15:49:12 +0800

    fix: recover files that being corrupted in last commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:79fe31dad67ab64a73a40f9db3760c467a3d3ce6262843a9adc144b39f670d1a
-size 61
+# Guiding DIP Early Stopping with DDPM-inspired Supervision
--- models/DnCNN.py
+++ models/DnCNN.py
@@ -1,3 +1,30 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa31df111ca9257b2abce7523f0bb5292a6baaac51e596698e04773d57fa453a
-size 981
+import torch
+import torch.nn as nn
+
+class DnCNN(nn.Module):
+    def __init__(
+            self,
+            num_of_channel: int=1,
+            features: int=64,
+            num_of_layers: int=32,
+            kernel_size: int=3,
+            padding: int=1
+        ):
+        super(DnCNN, self).__init__()
+
+        layers = [
+            nn.Conv2d(num_of_channel, features, kernel_size=kernel_size, padding=padding, bias=False),
+            nn.ReLU(inplace=True)
+        ]
+        for i in range(num_of_layers-2):
+            layers.append(nn.Conv2d(features, features, kernel_size=kernel_size, padding=padding, bias=False))
+            layers.append(nn.BatchNorm2d(features))
+            layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Conv2d(features, num_of_channel, kernel_size, padding=padding, bias=False))
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.network(x)
+        return out
+    
